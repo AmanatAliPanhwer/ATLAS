a83c68b3b056c7f3bdbbd60938d7082fb7c020ec
Amanat Ali <alph702.12@gmail.com>
2026-03-23 15:35:03 +0500

    docs: updated README

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import uvicorn
 from Brain.RTC import RTC
 from dotenv import load_dotenv
 import tracemalloc
+import subprocess
 
 load_dotenv('.env')
 tracemalloc.start()
@@ -15,5 +16,10 @@ async def websocket_endpoint(websocket: WebSocket):
     rtc = RTC()
     await rtc.run(websocket)
 
+def start_frontend():
+    # Start Electron frontend as a subprocess
+    subprocess.Popen(["npm", "start"], cwd="./frontend/", shell=True)
+
 if __name__ == "__main__":
+    start_frontend()
     uvicorn.run(app, host="0.0.0.0", port=8000)
